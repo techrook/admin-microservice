6829e252e2c6001cfeba07c4fcaffe0f40bcb398
techrook <mondayitohowo3@gmail.com>
2024-06-22 15:03:23 +0100

    db connection

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,12 +10,26 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const product_module_1 = require("./product/product.module");
+const typeorm_1 = require("@nestjs/typeorm");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'mysql',
+                host: 'localhost',
+                port: 3306,
+                username: 'root',
+                password: 'root',
+                database: 'nest_admin',
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+            product_module_1.ProductModule
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
